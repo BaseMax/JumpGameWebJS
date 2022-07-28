e8bac10d8d473de9e0fc88ea475b21172868a285
Max Base <maxbasecode@gmail.com>
2022-07-28 14:27:02 +0200

    update

--- script.js
+++ script.js
@@ -46,31 +46,25 @@ function generateObstacles() {
     obstacle.classList.add("obstacle");
     obstacle.style.left = groundRight + groundWidth + "px";
     obstacles.appendChild(obstacle);
-    let obstacleBottom = parseInt(window.getComputedStyle(obstacle).getPropertyValue('bottom'));
-    let obstacleRight = parseInt(window.getComputedStyle(obstacle).getPropertyValue('right'));
-    let obstacleWidth = parseInt(window.getComputedStyle(obstacle).getPropertyValue('width'));
-    let obstacleHeight = parseInt(window.getComputedStyle(obstacle).getPropertyValue('height'));
-    let obstacleSpeed = Math.floor(Math.random() * 5) + 1;
-    let obstacleInterval = setInterval(() => {
-        obstacleRight -= obstacleSpeed;
+
+    let obstacleRight = -30;
+    let obstacleBottom = 100;
+    let obstacleWidth = 30;
+    let obstacleHeight = Math.floor(Math.random() * 50) + 50;
+    obstacle.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+
+    function moveObject() {
+        obstacleRight += 5;
         obstacle.style.right = obstacleRight + "px";
-        if (obstacleRight <= -obstacleWidth) {
-            obstacles.removeChild(obstacle);
-            clearInterval(obstacleInterval);
-        }
-    }, obstacleSpeed);
-
-    let isColliding = false;
-    let collisionInterval = setInterval(() => {
-        if (characterRight + characterWidth >= obstacleRight && characterRight <= obstacleRight + obstacleWidth && characterBottom + characterHeight >= obstacleBottom && characterBottom <= obstacleBottom + obstacleHeight) {
-            isColliding = true;
-            clearInterval(collisionInterval);
-        }
-    }, 10);
-
-    if (isColliding) {
-        alert("Game Over");
-        location.reload();
+        obstacle.style.bottom = obstacleBottom + "px";
+        obstacle.style.width = obstacleWidth + "px";
+        obstacle.style.height = obstacleHeight + "px";
     }
-    
+
+    let obstacleInterval = setInterval(moveObject, 10*2);
+    let obstacleTimeout = setTimeout(() => {
+        clearInterval(obstacleInterval);
+        obstacles.removeChild(obstacle);
+    }, groundRight + groundWidth + obstacleRight);
+
 }
