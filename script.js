73bab430464ae1a09a4fc5777495acb41967f189
Max Base <maxbasecode@gmail.com>
2022-07-28 14:22:29 +0200

    adding obstacle

--- script.js
+++ script.js
@@ -11,3 +11,66 @@ let groundWidth = parseInt(window.getComputedStyle(ground).getPropertyValue('wid
 let isJumping = false;
 let upTime;
 let downTime;
+
+function jump() {
+    if (isJumping) return;
+    upTime = setInterval(() => {
+        if (characterBottom >= groundHeight + 250) {
+            clearInterval(upTime);
+            downTime = setInterval(() => {
+                if (characterBottom <= groundHeight) {
+                    clearInterval(downTime);
+                    isJumping = false;
+                }
+                characterBottom -= 10;
+                character.style.bottom = characterBottom + "px";
+            }, 10);
+        }
+
+        characterBottom -= 10;
+        character.style.bottom = characterBottom + "px";
+        isJumping = true;
+    }, 20);
+}
+
+function control(e) {
+    if (e.key === "ArrowUp" || e.key === ' ') {
+        jump();
+    }
+
+}
+
+function generateObstacles() {
+    let obstacles = document.querySelector(".obstacles");
+    let obstacle = document.createElement("div");
+    obstacle.classList.add("obstacle");
+    obstacle.style.left = groundRight + groundWidth + "px";
+    obstacles.appendChild(obstacle);
+    let obstacleBottom = parseInt(window.getComputedStyle(obstacle).getPropertyValue('bottom'));
+    let obstacleRight = parseInt(window.getComputedStyle(obstacle).getPropertyValue('right'));
+    let obstacleWidth = parseInt(window.getComputedStyle(obstacle).getPropertyValue('width'));
+    let obstacleHeight = parseInt(window.getComputedStyle(obstacle).getPropertyValue('height'));
+    let obstacleSpeed = Math.floor(Math.random() * 5) + 1;
+    let obstacleInterval = setInterval(() => {
+        obstacleRight -= obstacleSpeed;
+        obstacle.style.right = obstacleRight + "px";
+        if (obstacleRight <= -obstacleWidth) {
+            obstacles.removeChild(obstacle);
+            clearInterval(obstacleInterval);
+        }
+    }, obstacleSpeed);
+
+    let isColliding = false;
+    let collisionInterval = setInterval(() => {
+        if (characterRight + characterWidth >= obstacleRight && characterRight <= obstacleRight + obstacleWidth && characterBottom + characterHeight >= obstacleBottom && characterBottom <= obstacleBottom + obstacleHeight) {
+            isColliding = true;
+            clearInterval(collisionInterval);
+        }
+    }, 10);
+
+    if (isColliding) {
+        alert("Game Over");
+        location.reload();
+    }
+    
+}
